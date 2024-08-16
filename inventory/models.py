0b40e826bbc4fd0f0005fdb7c7152901807f0234
Eki <mormileenzo1@gmail.com>
2024-08-16 17:23:55 -0300

    db_schema

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Refrigerator(models.Model):
+    name = models.CharField(max_length=100)
+
+class Column(models.Model):
+    refrigerator = models.ForeignKey(Refrigerator, on_delete=models.CASCADE)
+
+class Cut(models.Model):
+    milas_per_tupper = models.IntegerField()
+
+class SubColumn(models.Model):
+    column = models.ForeignKey(Column, on_delete=models.CASCADE)
+    cut = models.ForeignKey(Cut, on_delete=models.CASCADE)
+    total_tuppers = models.IntegerField()
+    milas_tupper_in_use = models.IntegerField()
+    @property
+    def total_milanesas(self):
+        return self.total_tuppers * self.cut
+    def count(self):
+        return self.count()
--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from .models import SubColumn, Column, Refrigerator, Cut
 # Create your tests here.
+
+class TestDB(TestCase):
+    def setUp(self):
+        SubColumn.objects.create(
+            column=Column.objects.create(
+                refrigerator=Refrigerator.objects.create(name="TestRefrigerator")
+                ),
+            cut=Cut.objects.create(milas_per_tupper=10),
+            total_tuppers=18,
+            milas_tupper_in_use=10
+        )
+    
+    def test(self):
+        subcolumn = SubColumn.objects.get(id=1)
+        print(subcolumn)
