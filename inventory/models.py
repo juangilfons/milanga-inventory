b62fed99d0d88d1dc676d9ac80af0dcbf451c1c4
Eki <mormileenzo1@gmail.com>
2024-08-30 19:31:30 -0300

    db_schema 2.0

--- inventory/models.py
+++ inventory/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from dataclasses import dataclass
+from itertools import zip_longest, repeat
 
 # Create your models here.
 class Refrigerator(models.Model):
     name = models.CharField(max_length=100)
+
 class Column(models.Model):
     refrigerator = models.ForeignKey(Refrigerator, on_delete=models.CASCADE)
+    def subcolumn_generator(self):
+        subcolumns = SubColumn.objects.filter(total_tuppers__gt=0, column=self)
+        total_tuppers_list = subcolumns.values_list('total_tuppers', flat=True)
+        map_subcolumns_total_tuppers = dict(zip(subcolumns, total_tuppers_list))
+        groups = [iter(repeat(subcolumn, total_tuppers)) for subcolumn, total_tuppers in map_subcolumns_total_tuppers.items()]
+
+        for subcolumns in zip_longest(*groups, fillvalue=None):
+            for subcolumn in subcolumns:
+                if subcolumn is not None:
+                    yield subcolumn.cut
 
 class Cut(models.Model):
     name = models.CharField(max_length=100, default='test')
@@ -21,3 +34,9 @@ class SubColumn(models.Model):
     @classmethod
     def search(cls, c):
         return cls.objects.filter(cut_name=c).count()
+
+@dataclass
+class Tupper:
+   def __init__(self, subcolumn):
+       self.subcolumn = subcolumn
+       self.cut = subcolumn.cut
--- inventory/tests.py
+++ inventory/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import SubColumn, Column, Refrigerator, Cut
+from .models import SubColumn, Column, Refrigerator, Cut, Tupper
 # Create your tests here.
 
 class TestDB(TestCase):
@@ -9,18 +9,48 @@ class TestDB(TestCase):
                 refrigerator=Refrigerator.objects.create(name="TestRefrigerator")
                 ),
             cut=Cut.objects.create(name='Carne',milas_per_tupper=10),
-            total_tuppers=18,
-            milas_tupper_in_use=10
+            total_tuppers=2,
+            milas_tupper_in_use=4
         )
 
         SubColumn.objects.create(
-            column=Column.objects.create(
-                refrigerator=Refrigerator.objects.create(name="TestRefrigerator")
-                ),
-            cut=Cut.objects.create(name='Carne',milas_per_tupper=10),
-            total_tuppers=18,
-            milas_tupper_in_use=10
+            column=Column.objects.get(pk=1),
+            cut=Cut.objects.create(name='Pollo',milas_per_tupper=30),
+            total_tuppers=3,
+            milas_tupper_in_use=15
         )
+
+        SubColumn.objects.create(
+            column=Column.objects.get(pk=1),
+            cut=Cut.objects.create(name='Soja',milas_per_tupper=60),
+            total_tuppers=4,
+            milas_tupper_in_use=30
+        )
+
     
     def test(self):
-        self.assertEqual(SubColumn.search('Carne'), 2)
+        column=Column.objects.get(pk=1)
+        print(list(column.subcolumn_generator()))
+
+
+class CutModelTest(TestCase):
+    def setUp(self):
+        self.cut = Cut.objects.create(name="Carne", milas_per_tupper=10)
+
+    def test_create_cut(self):
+        self.assertEqual(self.cut.name, "Carne")
+
+    def test_read_cut(self):
+        cut = Cut.objects.get(id=self.cut.id)
+        self.assertEqual(cut.name, "Carne")
+
+    def test_update_cut(self):
+        self.cut.name = "Pollo"
+        self.cut.save()
+        updated_cut = Cut.objects.get(id=self.cut.id)
+        self.assertEqual(updated_cut.name, "Pollo")
+
+    def test_delete_cut(self):
+        self.cut.delete()
+        with self.assertRaises(Cut.DoesNotExist):
+            Cut.objects.get(id=self.cut.id)
