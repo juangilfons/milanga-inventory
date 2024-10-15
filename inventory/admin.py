fd83fe0863f4a8b84ac2e611d4ffabf56b6c9a85
Juan Gil <juangilfons@gmail.com>
2024-10-15 01:37:57 -0300

    Progress in inventory.html and inventory/views.py

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Refrigerator, Column, SubColumn, Cut
 
 # Register your models here.
+admin.site.register(Refrigerator)
+admin.site.register(Column)
+admin.site.register(SubColumn)
+admin.site.register(Cut)
--- inventory/views.py
+++ inventory/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from inventory.models import Refrigerator
+
+
 # Create your views here.
 
 @login_required
 def inventory(request):
-    return render(request, 'inventory/inventory.html')
+    refrigerators = Refrigerator.objects.prefetch_related('column_set__subcolumn_set')
+    context = {
+        'refrigerators': refrigerators,
+    }
+    return render(request, 'inventory/inventory.html', context)
